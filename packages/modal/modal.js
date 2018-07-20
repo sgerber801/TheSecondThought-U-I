4e1b46cc34f8ff004b2f818244e867b06b54d464
Steve Gerber <sgerber801@hotmail.com>
2018-07-19 20:35:48 -0600

    package changes

---
+++ packages/modal/modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { Container } from './styles'
+
+class ModalEntry extends Component {
+  render(){
+    return (
+      <Container>
+        Hello from the Modal
+      </Container>  
+    )
+  }
+}
+
+export default ModalEntry
